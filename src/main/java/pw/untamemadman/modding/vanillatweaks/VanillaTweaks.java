109d9570c2538d3317279b0e45597fe710954bf7
untamemadman <sam@untamemadman.pw>
2015-02-21 12:16:56 +0000

    Started work. Done basics of main mod file, references and Proxy's

---
+++ src/main/java/pw/untamemadman/modding/vanillatweaks/VanillaTweaks.java
@@ -0,0 +1,41 @@
+package pw.untamemadman.modding.vanillatweaks;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import pw.untamemadman.modding.vanillatweaks.proxy.CommonProxy;
+import pw.untamemadman.modding.vanillatweaks.reference.Reference;
+
+/**
+ * Created by untamemadman on 2/21/2015.
+ */
+
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION, guiFactory = Reference.GUIFactoryClass)
+public class VanillaTweaks
+{
+    @Mod.Instance(Reference.MOD_ID)
+    public static VanillaTweaks instances;
+
+    @SidedProxy(clientSide = Reference.ClientProxyClass, serverSide = Reference.ServerProxyClass)
+    public static CommonProxy proxy;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent even)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void Init (FMLInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit (FMLPostInitializationEvent even)
+    {
+
+    }
+}
